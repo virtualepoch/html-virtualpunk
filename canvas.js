efa3ab366d5bd43fb55797ba0cad861495272e2a
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-01-30 22:55:58 -0700

    canvas mobile testing

--- canvas.js
+++ canvas.js
@@ -1,21 +1,31 @@
+// HTML ELEMENTS
 const canvas = document.getElementById("canvas1");
+const inputSidesValue = document.querySelector("#input-sides").value;
+
+document.querySelector("#input-sides").addEventListener("keydown", function (e) {
+  if (e.code === "Enter") branches = inputSidesValue;
+});
+
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const maxLevel = 5;
+const maxLevel = 3;
 const branches = 2;
-const sides = Math.floor(Math.random() * 10 + 3);
+// const sides = Math.floor(Math.random() * 10) + 3);
+const sides = 4;
 const spread = Math.random() * 48 + 0.51;
+
+console.log(Math.random * 48);
 
 ctx.translate(canvas.width / 2, canvas.height / 2);
 
-const angle = Math.PI * 2 * spread;
+const angle = Math.PI * 2 * 0.85;
 
 function drawLine(level) {
   if (level > maxLevel) return;
 
-  ctx.strokeStyle = "rgb(255,0,0)";
+  ctx.strokeStyle = "rgb(0,255,255)";
   ctx.lineWidth = 2;
   ctx.beginPath();
   ctx.moveTo(0, 0);
@@ -50,10 +60,10 @@ function drawSnowflake() {
 
 drawSnowflake();
 
-function randomizeSnowflake(){
-    sides = Math.floor(Math.random() * 7 + 3);
-    spread = Math.random() * 0.6 - 0.3;
-    drawSnowflake();
-}
+// function randomizeSnowflake() {
+//   sides = Math.floor(Math.random() * 7 + 3);
+//   spread = Math.random() * 0.6 - 0.3;
+//   drawSnowflake();
+// }
 
 const buttonRandom = document.querySelector(".button-canvas").addEventListener("click", randomizeSnowflake());
